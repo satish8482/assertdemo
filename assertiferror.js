c1be78b38420d152c3914fa9a56d106c9972acd3
satish <satish.8@gtcs.com>
2017-10-17 10:53:37 +0530

    deepEqual Example

--- assertiferror.js
+++ assertiferror.js
@@ -20,9 +20,9 @@ const assert = require('assert');
 
 assert.ifError(0);
 // OK
-assert.ifError(1);
+//assert.ifError(1);
 // Throws 1
-assert.ifError('error');
+//assert.ifError('error');
 // Throws 'error'
-assert.ifError(new Error());
+//assert.ifError(new Error());
 // Throws Error
